82025d24384bb46b14e33efa08bea20fc6e85752
fanlia <3093932086@qq.com>
2022-02-17 22:24:05 +0800

    first commit

---
+++ graphql.js
@@ -0,0 +1,30 @@
+
+const { graphiql } = require('graphql-simple')
+const GraphiqlAuth = require('@ersinfotech/graphiql-auth')
+
+module.exports = (app, config, {schema, api, resolver, gql = []}, restrict) => {
+
+    const graphLogin = (callbackUrl) => GraphiqlAuth({
+      eadminBaseUrl: config.eadmin.baseUrl,
+      clientId: config.clientId,
+      callbackUrl,
+    })
+
+    const graphiqlGetNotFound = (req, res, next) => {
+      const re = new RegExp(`${req.baseUrl}/login$`)
+      if (req.method === 'GET' && !re.test(req.headers.referer)) {
+        return res.status(404).end('Not Found')
+      }
+      next()
+    }
+
+    const graphin = graphLogin('../graphql')
+
+    app.route('/login')
+    .get(graphin.get)
+    .post(graphin.post)
+
+    const graphql = graphiql({schema, api, resolver})
+
+    app.use('/', graphiqlGetNotFound, restrict(), gql, graphql)
+}
